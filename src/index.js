15c3e412171982a184266ce9fd8b5a74872ce7fd
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-11-14 14:24:02 +0300

    14 ноября

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 const tmpl = '<form class="card_validation_form"><input class="card_number" type="text" placeholder="Enter your card ID" maxlength="16" autofocus> <button class="validation_button">Click to Validate</button></form>';
-import { checkCardSystem } from './checkcardsystem';
-import { checkValidity } from './checkvalidity';
+import { validateCardNumber } from './validatecardnumber';
+import { getCardSystem } from './getcardsystem';
 
 
 const init = () => {
@@ -9,6 +9,29 @@ const init = () => {
     divEl.innerHTML = tmpl;
     const formEl = divEl.querySelector('.card_validation_form');
     const inputEl = divEl.querySelector('.card_number');
+
+    function checkValidity(event) {
+        event.preventDefault();
+        const cardNumber = inputEl.value;
+        const result = validateCardNumber(cardNumber);
+        if (result.success) {
+            alert(result.message)
+        }
+    };
+
+    function checkCardSystem() {
+        const cardNumber = inputEl.value;
+        const cardSystem = getCardSystem(cardNumber);
+        const cardIconsEl = Array.from(document.querySelectorAll('.card-icon'));
+        for (let cardIconEl of cardIconsEl) {
+            if (cardIconEl.classList.contains(cardSystem)) {
+                cardIconEl.classList.toggle('grey-icon');
+                console.log(cardIconEl)
+            } else {
+                cardIconEl.classList.add('grey-icon');
+            }
+        }
+    };    
 
     inputEl.addEventListener('input', checkCardSystem);
     inputEl.addEventListener('paste', checkCardSystem);
