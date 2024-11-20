98dfa5ed704376c594e845b9866700b5baa52bc3
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-11-20 16:35:15 +0300

    Финал

--- src/__tests__/checkvalidity.test.js
+++ src/__tests__/checkvalidity.test.js
@@ -2,21 +2,30 @@
  * @jest-environment jsdom
  */
 
+import { init } from "../init";
 
 test('alert validity status', () => {
-    const formEl = document.createElement('form');
-    const inputEl = document.createElement('input');
-    inputEl.style.type = 'text';
-    formEl.appendChild(inputEl);
-    document.body.appendChild(formEl);
+    const mainEl = document.createElement('div');
+    mainEl.classList.add('main');
+    document.body.append(mainEl);
 
-    const alert = jest.fn(() => {
-        return 'Форма работает'
-    });
+    init();
 
-    formEl.addEventListener('submit', alert);
-    const submit = new Event('submit');
-    let result = formEl.dispatchEvent(submit);
-    let answer = 'Форма работает';
-    expect(result).toBe(answer);
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const formEl = mainEl.querySelector('.card_validation_form');
+    const inputEl = mainEl.querySelector('.card_number');
+    const testCardNumber = '4111111111111111';
+    inputEl.value = testCardNumber;
+
+    expect(alertMock).toHaveBeenCalledTimes(0);
+
+    const submitEvent = new Event('submit');
+    formEl.dispatchEvent(submitEvent);
+
+    expect(alertMock).toHaveBeenCalledWith('Этот номер валиден');
+    expect(alertMock).toHaveBeenCalledTimes(1);
+
+    alertMock.mockRestore();
+    
 });
--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
 import './index.css';
-const tmpl = '<form class="card_validation_form"><input class="card_number" type="text" placeholder="Enter your card ID" maxlength="16" autofocus> <button class="validation_button">Click to Validate</button></form>';
-import { validateCardNumber } from './validatecardnumber';
-import { getCardSystem } from './getcardsystem';
+import { init } from './init';
 
-
-const init = () => {
-    const divEl = document.createElement('div');
-    divEl.innerHTML = tmpl;
-    const formEl = divEl.querySelector('.card_validation_form');
-    const inputEl = divEl.querySelector('.card_number');
-
-    function checkValidity(event) {
-        event.preventDefault();
-        const cardNumber = inputEl.value;
-        const result = validateCardNumber(cardNumber);
-        if (result.success) {
-            alert(result.message)
-        }
-    };
-
-    function checkCardSystem() {
-        const cardNumber = inputEl.value;
-        const cardSystem = getCardSystem(cardNumber);
-        const cardIconsEl = Array.from(document.querySelectorAll('.card-icon'));
-        for (let cardIconEl of cardIconsEl) {
-            if (cardIconEl.classList.contains(cardSystem)) {
-                cardIconEl.classList.toggle('grey-icon');
-                console.log(cardIconEl)
-            } else {
-                cardIconEl.classList.add('grey-icon');
-            }
-        }
-    };    
-
-    inputEl.addEventListener('input', checkCardSystem);
-    inputEl.addEventListener('paste', checkCardSystem);
-    formEl.addEventListener('submit', checkValidity);
-
-    const main = document.querySelector('.main');
-    main.appendChild(divEl);
-};
 init();
 
